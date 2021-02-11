ee9e19ed3a5102d60e09d0594b752a628946350d
nihadrahman <nihad1999@hotmail.co.uk>
2021-02-11 20:45:42 +0000

    Add Functionality

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'Mm0OTWLEtJYVmtX0PT3C8Uh2FxJ3eu0O'
 
 
 @app.route('/')
@@ -10,7 +13,19 @@ def home():
 
 @app.route('/about')
 def about():
-    return 'About!'
+    return render_template('about.html')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='login', form=form)
 
 
 if __name__ == '__main':
