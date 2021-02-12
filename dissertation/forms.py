2a3f47d80a2094256b895379bf26f3d1121911bd
nihadrahman <nihad1999@hotmail.co.uk>
2021-02-12 19:51:19 +0000

    Add User Authentication

--- dissertation/forms.py
+++ dissertation/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from dissertation.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -12,6 +13,16 @@ class RegistrationForm(FlaskForm):
         'Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already taken.')
 
 
 class LoginForm(FlaskForm):
--- dissertation/models.py
+++ dissertation/models.py
@@ -1,18 +1,25 @@
-from dissertation import db
+from dissertation import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    test_taken = db.Column(db.Boolean, unique=False, default=False)
     pretest_result = db.Column(db.Integer, nullable=True)
     courses = db.relationship('Course', backref='student_id', lazy=True)
     bnmodels = db.relationship('BNModel', backref='student_id', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.username}', '{self.pretest_result}')"
+        return f"User('{self.username}', '{self.email}', '{self.test_taken}', '{self.pretest_result}')"
 
 
 class Topic(db.Model):
