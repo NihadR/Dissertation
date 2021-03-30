7dbba7033c3a3393689cdae0a4f61a3382cb1e9c
nihadrahman <nihad1999@hotmail.co.uk>
2021-03-30 18:11:24 +0100

    Main package comments

--- dissertation/main/routes.py
+++ dissertation/main/routes.py
@@ -7,10 +7,17 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @main.route('/home')
 def home():
+    '''
+    Home page 
+    Runs the model when first landing on the page 
+    '''
     runmodel()
     return render_template('main/home.html')
 
 
 @main.route('/about')
 def about():
+    '''
+    About page, renders a template providing information to the user 
+    '''
     return render_template('main/about.html')
